99421d116fda645f16f461c2f344b41f9609f653
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-09-16 17:50:47 +0700

    finish show hide add book dialog

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -1,1 +1,12 @@
 // Your JS code goes here
+const addBookDialogDOM = document.querySelector('#add-book-dialog');
+const addBookBtnDOM = document.querySelector('#add-book-btn');
+const closeAddBookDialogIconDOM = document.querySelector('#close-add-book-dialog');
+
+addBookBtnDOM.addEventListener('click', () => {
+    addBookDialogDOM.style.display = 'flex';
+})
+
+closeAddBookDialogIconDOM.addEventListener('click', () => {
+    addBookDialogDOM.style.display = 'none';
+})
