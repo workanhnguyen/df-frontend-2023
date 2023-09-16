08cd8fe03fe3864d99da138d75362cb464686dbd
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-09-16 18:36:33 +0700

    finish show delete dialog

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -1,12 +1,57 @@
 // Your JS code goes here
-const addBookDialogDOM = document.querySelector('#add-book-dialog');
-const addBookBtnDOM = document.querySelector('#add-book-btn');
-const closeAddBookDialogIconDOM = document.querySelector('#close-add-book-dialog');
+const addBookDialogDOM = document.querySelector("#add-book-dialog");
+const deleteBookDialogDOM = document.querySelector("#delete-book-dialog");
 
-addBookBtnDOM.addEventListener('click', () => {
-    addBookDialogDOM.style.display = 'flex';
-})
+const dataZoneDOM = document.querySelector("#data-zone");
 
-closeAddBookDialogIconDOM.addEventListener('click', () => {
-    addBookDialogDOM.style.display = 'none';
-})
+function showAddBookDialog() {
+    addBookDialogDOM.style.display = "flex";
+}
+
+function hideAddBookDialog() {
+    addBookDialogDOM.style.display = "none";
+}
+
+function showDeleteBookDialog() {
+    deleteBookDialogDOM.style.display = "flex";
+}
+
+function hideDeleteBookDialog() {
+    deleteBookDialogDOM.style.display = "none";
+}
+
+function onLoad() {
+  const sampleData = [
+    {
+      name: "Refactoring",
+      author: "Martin Fowler",
+      topic: "Programming",
+    },
+    {
+      name: "Designing Data-Intensive Applications",
+      author: "Martin Kleppmann",
+      topic: "Database",
+    },
+    {
+      name: "The Phoenix Project",
+      author: "Gene Kim",
+      topic: "DevOps",
+    },
+  ];
+
+  localStorage.setItem("data", JSON.stringify(sampleData));
+
+  for (const data of sampleData) {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+        <td>${data.name}</td>
+        <td>${data.author}</td>
+        <td>${data.topic}</td>
+        <td onclick='showDeleteBookDialog()' class='text-danger text-underline'>Delete</td>
+      `;
+
+    dataZoneDOM.appendChild(row);
+  }
+}
+
+onLoad();
