10c139f7718d371fd384169c1cef8b58e483a2c7
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-10-02 17:29:20 +0700

    finish search with keywork

--- assignment-4/src/components/Pagination.tsx
+++ assignment-4/src/components/Pagination.tsx
@@ -4,8 +4,11 @@ import React, { useEffect, useState, memo } from 'react'
 import { useStateContext } from '../contexts/ContextProvider'
 import { BOOK_PER_PAGE } from '../constants/constants'
 import { Book } from '../models/Book'
+import { useSearchParams } from 'next/navigation'
 
 const Pagination: React.FC = () => {
+  const searchParams = useSearchParams();
+
   const context = useStateContext()
   const books: Book[] | undefined = context?.books
   const bookBySearch: Book[] | undefined = context?.bookBySearch
@@ -19,12 +22,18 @@ const Pagination: React.FC = () => {
     setBookCount(Math.ceil(bookBySearch?.length! / BOOK_PER_PAGE))
   }, [books, bookBySearch])
 
+  const handleChangePage = (pageNumber: number) => {
+    setPageIndex && setPageIndex(pageNumber)
+    // router.push(`${router}`)
+    console.log(searchParams.entries());
+  }
+
   const pageNumbers = Array.from({ length: bookCount }, (_, index) => index + 1)
   return (
     <div className="w-full flex justify-end mt-10 gap-2">
       {pageNumbers.map((pageNumber) => (
         <button
-          onClick={() => setPageIndex!(pageNumber)}
+          onClick={() => handleChangePage(pageNumber)}
           className={`px-2 rounded-sm cursor-pointer ${
             pageIndex === pageNumber && 'bg-primary text-white'
           }`}
--- assignment-4/src/components/Search.tsx
+++ assignment-4/src/components/Search.tsx
@@ -1,36 +1,72 @@
 'use client'
 
-import React, { useEffect, useState } from "react";
+import React, { FormEvent, useEffect, useState } from 'react'
 
-import searchIcon from "../assets/search-icon.svg";
-import { useStateContext } from "../contexts/ContextProvider";
-import Image from "next/image";
+import searchIcon from '../assets/search-icon.svg'
+import { useStateContext } from '../contexts/ContextProvider'
+import Image from 'next/image'
+import { useRouter, useSearchParams } from 'next/navigation'
 
 const Search: React.FC = () => {
-  const context = useStateContext();
-  const books = context?.books;
-  const setBookBySearch = context?.setBookBySearch;
-  const setPageIndex = context?.setPageIndex;
+  const router = useRouter()
+  const searchParams = useSearchParams()
 
-  const [keyword, setKeyword] = useState("");
+  const context = useStateContext()
+  const books = context?.books
+  const setBookBySearch = context?.setBookBySearch
+  const setPageIndex = context?.setPageIndex
+
+  const [keyword, setKeyword] = useState('')
 
   useEffect(() => {
-    setBookBySearch && setBookBySearch(books ? books!.filter((book) => book.name.toLowerCase().includes(keyword.toLowerCase())) : []);
-    setPageIndex && setPageIndex!(1);
-  }, [keyword, books]);
+    let kw = keyword
+    if (searchParams.get('q') !== null) kw = searchParams.get('q') as string
+
+    setKeyword(kw)
+
+    setBookBySearch &&
+      setBookBySearch(
+        books
+          ? books!.filter((book) =>
+              book.name.toLowerCase().includes(kw.toLowerCase()),
+            )
+          : [],
+      )
+    setPageIndex && setPageIndex!(1)
+  }, [])
+
+  const handleSearch = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault()
+
+    setBookBySearch &&
+      setBookBySearch(
+        books
+          ? books!.filter((book) =>
+              book.name.toLowerCase().includes(keyword.toLowerCase()),
+            )
+          : [],
+      )
+    setPageIndex && setPageIndex!(1)
+
+    if (keyword !== '') router.push(`?q=${keyword}`)
+    else router.push('/')
+  }
 
   return (
-    <div className="flex items-center border-2 bg-white gap-1 px-2 rounded-md overflow-hidden">
+    <form
+      onSubmit={handleSearch}
+      className="flex items-center border-2 bg-white gap-1 px-2 rounded-md overflow-hidden"
+    >
       <Image className="w-4 h-4 text-gray" src={searchIcon} alt="search-icon" />
       <input
         value={keyword}
         onChange={(e) => setKeyword(e.target.value)}
         className="border-none outline-none px-1 py-2"
         type="text"
-        placeholder="Search books..."
+        placeholder="Type and enter..."
       />
-    </div>
-  );
-};
+    </form>
+  )
+}
 
-export default Search;
+export default Search
